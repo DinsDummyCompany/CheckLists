eff654e3edee05a172f12ba3218ef34412a06f31
Jonathan Carnie <jono.carnie@gmail.com>
2012-08-28 20:50:46 +1000

    Added more code to manage cells being selected and deselected.

--- CheckLists/ViewController.m
+++ CheckLists/ViewController.m
@@ -40,6 +40,37 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CheckListItem"];
+    
+    UILabel *itemLabel = (UILabel *) [cell viewWithTag:(1000)];
+    
+    if (indexPath.row == 0){
+        itemLabel.text = @"Walk the dog";
+    }else if (indexPath.row == 1){
+        itemLabel.text = @"Wash the Car";
+    }else if (indexPath.row == 2){
+        itemLabel.text = @"Put out the clothes";
+    }else if (indexPath.row == 3){
+        itemLabel.text = @"Brush my Teeth";
+    }else if (indexPath.row == 4){
+        itemLabel.text = @"Play games";
+    }
+        
     return cell;
 }
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if (selectedCell.accessoryType == UITableViewCellAccessoryNone) {
+        selectedCell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        selectedCell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
